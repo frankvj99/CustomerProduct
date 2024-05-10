7b47818c29648fd395c538113707d6fca2b9fe2d
Frank Romeo <frankvj99@yahoo.com>
2024-05-10 10:46:47 -0400

    Add product tests.

---
+++ test/Interview.Data.Test/AutoMapper/ProductProfileTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using AutoMapper;
+using FluentAssertions;
+using Interview.Data.AutoMapper;
+using Interview.Data.Entities;
+using Interview.Data.Models;
+using Xunit;
+
+namespace Interview.Data.Test.AutoMapper;
+
+public class ProductProfileTests
+{
+    private static readonly Fixture _fixture = new();
+
+    private readonly IMapper _mapper;
+
+    public ProductProfileTests()
+    {
+        _mapper = new MapperConfiguration(x => x.AddMaps(typeof(ProductProfile))).CreateMapper();
+    }
+
+    [Fact]
+    public void ShouldMapFromProductToProductEntity()
+    {
+        // Arrange
+        var product = _fixture.Create<Product>();
+
+        // Act
+        var productEntity = _mapper.Map<ProductEntity>(product);
+
+        // Assert
+        productEntity.Id.Should().Be(product.Id);
+        productEntity.Name.Should().Be(product.Name);   
+        productEntity.Description.Should().Be(product.Description);
+    }
+
+    [Fact]
+    public void ShouldMapFromProductEntityToProduct()
+    {
+        // Arrange
+        var productEntity = _fixture.Create<ProductEntity>();
+
+        // Act
+        var product = _mapper.Map<Product>(productEntity);
+
+        // Assert
+        product.Id.Should().Be(productEntity.Id);
+        product.Name.Should().Be(productEntity.Name);
+        product.Description.Should().Be(productEntity.Description);
+    }
+}
---
+++ test/Interview.Data.Test/Services/ProductServiceTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoMapper;
+using FluentAssertions;
+using Interview.Data.AutoMapper;
+using Interview.Data.Contexts;
+using Interview.Data.Entities;
+using Interview.Data.Models;
+using Interview.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Interview.Data.Test.Services;
+
+public class ProductServiceTests
+{
+    private static readonly Fixture _fixture = new();
+
+    private readonly InterviewContext _context;
+    private readonly IMapper _mapper;
+
+    private readonly ProductService _productService;
+
+    public ProductServiceTests()
+    {
+        var dbContextOptionsBuilder = new DbContextOptionsBuilder<InterviewContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+        _context = new InterviewContext(dbContextOptionsBuilder.Options);
+        _mapper = new MapperConfiguration(x => x.AddMaps(typeof(ProductProfile))).CreateMapper();
+        _productService = new ProductService(_context, _mapper);
+    }
+
+    [Fact]
+    public async Task AddOrUpdateProductAsync_ShouldUpdateNameIfMatchingProductExists()
+    {
+        // Arrange
+        var productEntity = _fixture.Build<ProductEntity>()
+            .With(x => x.Name, "Test Name 1")
+            .Create();
+
+        await _context.Products.AddAsync(productEntity);
+        await _context.SaveChangesAsync();
+
+        var product = new Product
+        {
+            Id = productEntity.Id,
+            Name = "Test Name 2"
+        };
+
+        // Act
+        await _productService.AddOrUpdateProductAsync(product);
+
+        // Assert
+        productEntity.Name.Should().Be("Test Name 2");
+
+        var productEntitiesCount = await _context.Products.CountAsync();
+        productEntitiesCount.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task AddOrUpdateProductAsync_ShouldUpdateDescriptionIfMatchingProductExists()
+    {
+        // Arrange
+        var productEntity = _fixture.Build<ProductEntity>()
+            .With(x => x.Description, "Test Description 1")
+            .Create();
+
+        await _context.Products.AddAsync(productEntity);
+        await _context.SaveChangesAsync();
+
+        var product = new Product
+        {
+            Id = productEntity.Id,
+            Description = "Test Description 2"
+        };
+
+        // Act
+        await _productService.AddOrUpdateProductAsync(product);
+
+        // Assert
+        productEntity.Description.Should().Be("Test Description 2");
+
+        var productEntitiesCount = await _context.Products.CountAsync();
+        productEntitiesCount.Should().Be(1);
+    }
+    
+    [Fact]
+    public async Task AddOrUpdateProductAsync_ShouldInsertNewProductEntityIfMatchingProductDoesNotExist()
+    {
+        // Arrange
+        var product = _fixture.Create<Product>();
+
+        // Act
+        await _productService.AddOrUpdateProductAsync(product);
+
+        // Assert
+        var productEntities = await _context.Products.ToListAsync();
+
+        productEntities.Should().HaveCount(1);
+
+        var productEntity = productEntities.Single();
+        productEntity.Id.Should().Be(product.Id);
+        productEntity.Name.Should().Be(product.Name);
+        productEntity.Description.Should().Be(product.Description);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData(0)]
+    public async Task GetProductAsync_ShouldReturnNullIfIdIsNullOr0(int id)
+    {
+        // Arrange & Act
+        var product = await _productService.GetProductAsync(id);
+
+        // Assert
+        product.Should().BeNull();
+    }
+
+}
